9890d0769239a1237203a118953981acc6594d1e
angelmz <angelsanchezx123x@gmail.com>
2022-12-26 15:24:55 -0600

    v2

---
+++ code_only/integers/v2/adder.ex
@@ -0,0 +1,11 @@
+defmodule Integers do
+  @moduledoc """
+  This module provides functions for adding integers.
+  Example:
+      iex> Integers.add(1, 2)
+      3
+  """
+  def adder(x, y) do
+    return x + y
+  end
+end
